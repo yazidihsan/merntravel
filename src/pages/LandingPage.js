283633d6e126128efc8eed02bc0cc9024a2ff423
yazidihsan <34444636+yazidihsan@users.noreply.github.com>
2023-02-07 10:13:13 +0700

    first_commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Button from "elements/Button";
+
+export default function LandingPage() {
+  return (
+    <View>
+      <Text>LandingPage</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
