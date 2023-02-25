d9e707179407aa95457b45d98a7bfd375776e6cf
yazidihsan <34444636+yazidihsan@users.noreply.github.com>
2023-02-25 14:33:00 +0700

    lanjutt

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,12 +2,14 @@ import Header from "parts/Header";
 import Hero from "parts/Hero";
 import React, { Component } from "react";
 import landingPage from "json/landingPage.json";
+import MostPicked from "parts/MostPicked";
 export default class LandingPage extends Component {
   render() {
     return (
       <>
         <Header {...this.props}></Header>
         <Hero data={landingPage.hero} />
+        <MostPicked data={landingPage.mostPicked} />
       </>
     );
   }
--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -5,6 +5,7 @@ import IcTravelers from "assets/images/icons/ic-traveler.svg";
 import IcTreasures from "assets/images/icons/ic-treasure.svg";
 import IcCities from "assets/images/icons/ic-cities.svg";
 import Button from "elements/Button";
+import formatNumber from "utils/formatNumber";
 
 export default function Hero(props) {
   function showMostPicked() {
@@ -13,6 +14,7 @@ export default function Hero(props) {
       behavior: "smooth",
     });
   }
+
   return (
     <section className="container pt-4">
       <div className="row align-items-center">
@@ -22,7 +24,10 @@ export default function Hero(props) {
             <br />
             Start Next Vacation
           </h1>
-          <p className="mb-5 font-weight-light text-gray-500 w-75">
+          <p
+            className="mb-4 font-weight-light text-gray-500 w-75"
+            style={{ lineHeight: "170%" }}
+          >
             We provide what you need to enjoy your holiday with family. Time to
             make another memorable moments.
           </p>
@@ -34,35 +39,63 @@ export default function Hero(props) {
           >
             Show Me Now
           </Button>
-          <div className="row mt-5">
-            <div className="col-auto">
+          <div className="row" style={{ marginTop: 80 }}>
+            <div className="col-auto" style={{ marginRight: 35 }}>
               <img
                 src={IcTravelers}
+                width="36"
+                height="36"
                 alt={`${props.data.travelers} Travelers`}
               />
               <h6 className="mt-3">
-                {props.data.travelers}{" "}
+                {formatNumber(props.data.travelers)}{" "}
                 <span className="text-gray-500 font-weight-light">
                   travelers
                 </span>
               </h6>
             </div>
-            <div className="col-auto">
-              <img src={IcTreasures} alt={`${props.data.trasures} Treasures`} />
+            <div className="col-auto" style={{ marginRight: 35 }}>
+              <img
+                src={IcTreasures}
+                width="36"
+                height="36"
+                alt={`${props.data.trasures} Treasures`}
+              />
               <h6 className="mt-3">
-                {props.data.treasures}{" "}
+                {formatNumber(props.data.treasures)}{" "}
                 <span className="text-gray-500 font-weight-light">
                   treasures
                 </span>
               </h6>
             </div>
             <div className="col-auto">
-              <img src={IcCities} alt={`${props.data.cities} Travelers`} />
+              <img
+                src={IcCities}
+                width="36"
+                height="36"
+                alt={`${props.data.cities} Travelers`}
+              />
               <h6 className="mt-3">
-                {props.data.cities}{" "}
+                {formatNumber(props.data.cities)}{" "}
                 <span className="text-gray-500 font-weight-light">cities</span>
               </h6>
             </div>
+          </div>
+        </div>
+        <div className="col-6 pl-5">
+          <div style={{ width: 520, height: 410 }}>
+            <img
+              src={ImgHero}
+              alt="Room with couches"
+              className="img-fluid position-absolute"
+              style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
+            />
+            <img
+              src={ImgHero_}
+              alt="Room with couches frame"
+              className="img-fluid position-absolute"
+              style={{ margin: "0 -15px -15px 0" }}
+            />
           </div>
         </div>
       </div>
