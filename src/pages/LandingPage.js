7f8c6809fe002735249dd94d0a27744fdbd42808
yazidihsan <34444636+yazidihsan@users.noreply.github.com>
2023-02-26 23:00:53 +0700

    fix mostpicked frontend

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,12 +4,19 @@ import React, { Component } from "react";
 import landingPage from "json/landingPage.json";
 import MostPicked from "parts/MostPicked";
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero data={landingPage.hero} />
-        <MostPicked data={landingPage.mostPicked} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
       </>
     );
   }
